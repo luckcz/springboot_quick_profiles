394a82fb0ca620c38ff5287ca172ed201e6d89ca
luckcz <1187117910@qq.com>
2018-10-31 09:27:37 +0800

    启动文件设置

---
+++ src/main/java/com/example/demo/SpringbootQuickProfilesApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootQuickProfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootQuickProfilesApplication.class, args);
+	}
+}
